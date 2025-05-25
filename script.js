ee854d6ca0a581bd4c497e826f712c908915c2bd
Joel hageman <joel@joelhageman.com>
2025-05-25 11:54:53 -0400

    Fixed Vercel env var injection

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ console.log("Phrase Bingo website loaded successfully!");
 
 // Initialize Firebase using environment variables
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: window.env.FIREBASE_API_KEY,
+  authDomain: window.env.FIREBASE_AUTH_DOMAIN,
+  projectId: window.env.FIREBASE_PROJECT_ID,
+  storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: window.env.FIREBASE_APP_ID,
+  measurementId: window.env.MEASUREMENT_ID
 };
 
 //debug:
